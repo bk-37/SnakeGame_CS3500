96e7cd601d17aca7e73ea9756c43146c754fa478
bk-37 <u1364562@umail.utah.edu>
2024-12-04 21:01:43 -0700

    added webserver project (full of bugs) and some sql capabilities to the gui (full of bugs)

---
+++ WebServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using CS3500.Networking;
+
+namespace WebServer
+{
+    public static class WebServer
+    {
+        private const string httpGoodHeader =
+            "HTTP/1.1 200 OK\r\n" +
+            "Connection: close\r\n" +
+            "Content-Type: text/html; charset=UTF-8\r\n" +
+            "\r\n";
+        private const string httpBadHeader =
+            "HTTP/1.1 404 Not Found\r\n" +
+            "Connection: close\r\n" +
+            "Content-Type: text/html; charset=UTF-8\r\n" +
+            "\r\n";
+        public static void Main(string[] args)
+        {
+            Server.StartServer(HandleConnection, 80);
+            Console.Read();
+        }
+
+        private static void HandleConnection(NetworkConnection connection)
+        {
+            string request = connection.ReadLine();
+            Debug.WriteLine(request);
+            if(request.Contains("GET / "))
+            {
+                connection.Send(httpGoodHeader + "<html>\r\n  <h3>Welcome to the Snake Games Database!</h3>\r\n  <a href=\"/games\">View Games</a>\r\n</html>");
+
+            }   
+            else if(request.Contains("GET /games"))
+            {
+                connection.Send(httpGoodHeader + "...");
+            }
+            //else if(request.Contains($"Get /games?gid={x}"))
+            //{
+            //    connection.Send(httpGoodHeaders  + "")
+            //}
+            connection.Disconnect();
+        }
+    }
+}
---
+++ WebServer/Server.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="Server.cs" company="UofU-CS3500">
+// Copyright (c) 2024 UofU-CS3500. All rights reserved.
+// </copyright>
+/// <authors>
+/// Brian Keller & Wyatt Young
+/// </authors>
+/// <version>
+/// November 3rd, 2024
+/// </version>
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CS3500.Networking;
+
+/// <summary>
+///   Represents a server task that waits for connections on a given
+///   port and calls the provided delegate when a connection is made.
+/// </summary>
+public static class Server
+{
+    /// <summary>
+    /// private static member variable for TCP listener initialized in StartServer. Null if server not started
+    /// </summary>
+    private static TcpListener? _listener;
+
+    /// <summary>
+    ///   Wait on a TcpListener for new connections. Alert the main program
+    ///   via a callback (delegate) mechanism.
+    /// </summary>
+    /// <param name="handleConnect">
+    ///   Handler for what the user wants to do when a connection is made.
+    ///   This should be run asynchronously via a new thread.
+    /// </param>
+    /// <param name="port"> The port (e.g., 11000) to listen on. </param>
+    public static void StartServer( Action<NetworkConnection> handleConnect, int port )
+    {
+        // create and start new TCP listener and initialize list of clients
+        _listener = new(IPAddress.Any, port);
+        _listener.Start();
+        // asynchronously accept connections into new threads
+        while (true) 
+        {
+            TcpClient client = _listener.AcceptTcpClient();
+            //clients.Add( client );
+            Console.WriteLine("Accepted new connection");
+            new Thread(() => handleConnect(new NetworkConnection(client))).Start();
+        }
+    }
+}
